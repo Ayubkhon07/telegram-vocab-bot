3bb06db26d4043b1946a8cbbc7f48762bd09fdb6
Ayubkhon07 <75295767+Ayubkhon07@users.noreply.github.com>
2025-04-16 16:42:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import math
 import json
 
 # Bot token (replace with your own)
-BOT_TOKEN = "PASTE_YOUR_TOKEN_HERE"
+BOT_TOKEN = "7575015472:AAE6RZZcJDeAMHaCMr62crpKULf5YJkq5Pw"
 
 # States
 ASK_COUNT, ASK_PERCENT, ASK_TIME = range(3)
